7042fcb834406d4adeb808d9a283b7aaf2b0cdbe
youngentry <90388461+youngentry@users.noreply.github.com>
2022-08-03 00:13:06 +0900

    220803

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
 $(function () {
-  $(".mainSlider").slick({});
+  $(".mainSlider").slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 10000,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    slidesToShow: 1,
+    cssEase: "linear",
+  });
+
+  $(".mainSlider").on("mouseenter", function (e) {
+    $("init .mainSlider").slick({
+      autoplay: false,
+      // pauseOnHover: true,
+    });
+    console.log("asd");
+  });
 
   $(".featureSlider").slick({});
+
+  var xCoor = $(window).on("mousemove", function (e) {
+    $(".mouseX").text("mouseX : " + e.pageX);
+    $(".mouseY").text("mouseY : " + e.pageY);
+    $(".mouseCircle").css("top", `${e.pageY - 10}px`);
+    $(".mouseCircle").css("left", `${e.pageX - 10}px`);
+  });
 });
